7976100fd767c6a723327f36b8f96d58f8bcab28
Gabrllo <gabriel.slettvoll@yahoo.com>
2022-11-15 17:09:18 +0100

    change charfield

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -6,7 +6,7 @@ class Car(models.Model):
     carmodel = models.CharField(max_length=50)
     year = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
-    status = models.CharField(max_length=45)
+    status = models.CharField(max_length=40)
     booked_by = models.CharField(max_length=50)
 
     def __str__(self):
